dbac5dd79e51c72038fe47350ec73f0294ebe6bb
Imílio Manuel <emiliomdmnauel12@gmail.com>
2024-10-19 01:52:50 +0100

    Criação do projecto Abapai e Configuração da Pagina Principal

---
+++ src/app/pages/initialpage/initialpage.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-initialpage',
+  standalone: true,
+  imports: [],
+  templateUrl: './initialpage.component.html',
+  styleUrl: './initialpage.component.scss'
+})
+export class InitialpageComponent {
+
+}
