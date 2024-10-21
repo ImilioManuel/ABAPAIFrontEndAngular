cf95429e018fa478111703b3f7c5c088d4f1977c
Imílio Manuel <emiliomdmnauel12@gmail.com>
2024-10-21 01:12:18 +0100

    Cabeálho feito

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
+import { BadgeModule } from 'primeng/badge';
+import { AvatarModule } from 'primeng/avatar';
+import { InputTextModule } from 'primeng/inputtext';
+import { CommonModule } from '@angular/common';
+import { RippleModule } from 'primeng/ripple';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [
+    MenubarModule,
+    BadgeModule, 
+    AvatarModule, 
+    InputTextModule, 
+    RippleModule, 
+    CommonModule
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+    items: MenuItem[] | undefined;
+
+    ngOnInit() {
+        this.items = [
+            {
+                label: 'Início',
+                icon: 'pi pi-home'
+            },
+            {
+                label: 'Minhas Provas',
+                icon: 'pi pi-book',
+                badge: '3',
+                items: [
+                    {
+                        label: 'Resultados',
+                        icon: 'pi pi-check-square'
+                    },
+                    {
+                        label: 'Fazer Prova',
+                        icon: 'pi pi-pencil'
+                    },
+                    {
+                        label: 'Ajuda',
+                        icon: 'pi pi-info-circle'
+                    },
+                ]
+            },
+            {
+                label: 'Relatórios de Desempenho',
+                icon: 'pi pi-chart-bar'
+            },
+            {
+                label: 'Minha Conta',
+                icon: 'pi pi-user',
+                items: [
+                    {
+                        label: 'Configurações',
+                        icon: 'pi pi-cog'
+                    },
+                    {
+                        label: 'Suporte Técnico',
+                        icon: 'pi pi-envelope'
+                    },
+                    {
+                        label: 'Sair',
+                        icon: 'pi pi-sign-out'
+                    },
+                ]
+            }
+        ];
+    }
 
 }
--- src/app/pages/initialpage/initialpage.component.ts
+++ src/app/pages/initialpage/initialpage.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-initialpage',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    TableModule,
+    CommonModule
+  ],
   templateUrl: './initialpage.component.html',
   styleUrl: './initialpage.component.scss'
 })
 export class InitialpageComponent {
+
 
 }
